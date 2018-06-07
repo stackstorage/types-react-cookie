a6c32e8832bf1350bacabe643e3005ec73b61eda
Fravic <fravicf@gmail.com>
2018-06-07 15:58:50 -0700

    Fix types for typescript 2.9

--- withCookies.d.ts
+++ withCookies.d.ts
@@ -2,7 +2,8 @@ import * as React from 'react';
 import { CookieComponentProps } from './CookiesProvider';
 
 // https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-311923766
-export type Diff<T extends string, U extends string> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T];
+type TKey = string | number | symbol;
+export type Diff<T extends TKey, U extends TKey> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T];
 export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
 
 export default function withCookies<P extends CookieComponentProps>(component: React.ComponentType<P>): React.ComponentClass<Omit<P, keyof CookieComponentProps>>;
